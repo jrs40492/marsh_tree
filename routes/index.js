4241b5cb4797cae0be31b33ba3de2d1d3af18635
Jacob Swanson <jrs40492@gmail.com>
2019-03-03 21:01:03 -0500

    Refactor styling to use modern CSS

--- routes/index.js
+++ routes/index.js
@@ -6,31 +6,41 @@ let data;
 
 router.get('/', async (req, res) => {
   res.render('index', {
-    node_env: process.env.NODE_ENV
+    node_env: process.env.NODE_ENV,
+    title: 'Home',
+    link: ''
   });
 });
 
 router.get('/about', async (req, res) => {
   res.render('about', {
     node_env: process.env.NODE_ENV,
+    title: 'About Us',
+    link: 'about'
   });
 });
 
 router.get('/tree_services', async (req, res) => {
   res.render('tree_services', {
     node_env: process.env.NODE_ENV,
+    title: 'Tree Services',
+    link: 'tree'
   });
 });
 
 router.get('/snow_services', async (req, res) => {
   res.render('snow_services', {
     node_env: process.env.NODE_ENV,
+    title: 'Snow Services',
+    link: 'snow'
   });
 });
 
 router.get('/contact', async (req, res) => {
   res.render('contact', {
     node_env: process.env.NODE_ENV,
+    title: 'Contact',
+    link: 'contact'
   });
 });
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,30 @@
 const path = require('path');
+const ETP = require('extract-text-webpack-plugin');
 
-module.exports = {
-  entry: './src/js/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist/js')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(s*)css&/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
+module.exports = [
+  {
+    entry: './src/js/index.js',
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist/js')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['babel-preset-env']
+            }
+          }
+        },
+        {
+          test: /\.scss$/,
+          use: ['style-loader', 'css-loader', 'sass-loader']
         }
-      }
-    ]
+      ]
+    }
   }
-};
+];
