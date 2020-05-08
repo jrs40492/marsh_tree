434a0cc3bbd3a043b1e2ad9eb57f68905a88790a
Jacob Swanson <jacobswanson@Jacobs-MacBook-Pro.local>
2020-05-08 18:05:07 -0400

    Update port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 
-const server = app.listen(4000, () => {
+const server = app.listen(80, () => {
   console.log(`Express is running on port ${server.address().port}`);
 });
